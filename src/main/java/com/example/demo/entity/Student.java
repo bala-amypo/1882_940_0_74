36f6add75238395abe2c52d8f2557ca265b0c7d7
bala-amypo <bala@amypo.in>
2025-12-18 14:32:37 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -1,60 +1,50 @@
 package com.example.demo.entity;
 
-import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Size;
-import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 @Entity
-// @Table(name = "students")
-public class Student {
-
+public class Stuentity{
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
-    @NotBlank(message="Name filed cannot be empty")
-    @Size(min=3,max=20,message="The user name must be min of 3 and max of 20 character")
     private String name;
-    @Email(message="Invalid email id")
-    @Column(unique=true)
     private String email;
+    private float cgpa;
 
-    // @NotNull
-    // @Size(min,max)
-    // @Min
-    // @Max
-    // @Pattern(reg)
-    // @Postive
-
-    public Long getId() {
+    public Long getId(){
         return id;
     }
-
-    public void setId(Long id) {
-        this.id = id;
+    public void setId(Long id){
+        this.id=id;
     }
-
-    public String getName() {
+    public String getName(){
         return name;
     }
-
-    public void setName(String name) {
-        this.name = name;
+    public void setName(String name){
+        this.name=name;
     }
-
-    public String getEmail() {
+    public String getEmail(){
         return email;
     }
-
-    public void setEmail(String email) {
-        this.email = email;
+    public void setEmail(String email){
+        this.email=email;
     }
-
-    public Student(String name,String email) {
-        this.name = name;
-        this.email = email;
+    public float getCgpa(){
+        return cgpa;
+    }
+    public void setCgpa(float cgpa){
+            this.cgpa=cgpa;
+    }
+    public Stuentity(Long id,String name,String email,float cgpa){
+      
+        this.name=name;
+        this.email=email;
+        this.cgpa=cgpa;
+    }
+    public Stuentity(){
+        
     }
 }
-    
-
-    
